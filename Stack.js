1c5d9c965e9c30666f3f2516be03174c1ebad710
HesamMirzaie <mirzaeeh08@gmail.com>
2024-08-05 19:15:02 +0330

    add pop method to stack

--- Stack.js
+++ Stack.js
@@ -1,33 +1,50 @@
 // implement the pop functionality
 
-
-
 class Node {
-    constructor(value){
-        this.value = value;
-        this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class Stack {
-    constructor(){
-        this.first = null;
-        this.last = null;
-        this.size = 0;
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
     }
-    push(val){
-        var newNode = new Node(val);
-        if(!this.first){
-            this.first = newNode;
-            this.last = newNode;
-        } else {
-            var temp = this.first;
-            this.first = newNode;
-            this.first.next = temp;
-        }
-        return ++this.size;
+    return ++this.size;
+  }
+  pop() {
+    // code here
+    if (!this.last) {
+      return undefined;
     }
-    pop(){
-        // code here
+    if (this.last === this.first) {
+      this.last = null;
+      this.size--;
     }
+    let temp = this.first;
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
 }
+const newStack = new Stack();
+newStack.push(12);
+newStack.push(15);
+newStack.push(17);
+newStack.push(19);
+console.log(newStack);
+newStack.pop();
+console.log(newStack);
