f385441f95b04f32c0b68df823a5e2dd1bd0e301
HesamMirzaie <mirzaeeh08@gmail.com>
2024-08-05 14:53:13 +0330

    better version

--- isSubsequence.js
+++ isSubsequence.js
@@ -38,3 +38,18 @@ console.log(
   isSubsequence('abc', 'abracadabra'), // true,
   isSubsequence('abc', 'acb') // false ,
 );
+
+// Get improved by AI
+function isSubsequence(str1, str2) {
+  let pointer1 = 0;
+  let pointer2 = 0;
+
+  while (pointer1 < str1.length && pointer2 < str2.length) {
+    if (str1[pointer1] === str2[pointer2]) {
+      pointer1++;
+    }
+    pointer2++;
+  }
+
+  return pointer1 === str1.length;
+}
