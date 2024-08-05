192ba6a0a1d5e47e484a0fada44f734c142e14f5
HesamMirzaie <mirzaeeh08@gmail.com>
2024-08-05 14:51:55 +0330

    solve ti

--- isSubsequence.js
+++ isSubsequence.js
@@ -15,6 +15,26 @@ Space Complexity - 0(1)
 
 **/
 
-function isSubsequence() {
-  // good luck. Add any arguments you deem necessary.
+function isSubsequence(str1, str2) {
+  let pointer1 = 0;
+  let pointer2 = 0;
+  // console.log(str1.length, str2.length);
+  while (pointer1 < str1.length && pointer2 < str2.length) {
+    if (str1[pointer1] === str2[pointer2]) {
+      pointer1++;
+      pointer2++;
+    } else if (str1[pointer1] !== str2[pointer2]) {
+      pointer2++;
+    }
+  }
+  if (pointer1 === str1.length) {
+    return true;
+  }
+  return false;
 }
+console.log(
+  isSubsequence('hello', 'hello world'), // true,
+  isSubsequence('sing', 'sting'), // true,
+  isSubsequence('abc', 'abracadabra'), // true,
+  isSubsequence('abc', 'acb') // false ,
+);
