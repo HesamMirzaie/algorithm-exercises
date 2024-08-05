8cd9019f67cd8adf0e2448663d1fe649dcd67cc7
HesamMirzaie <mirzaeeh08@gmail.com>
2024-08-05 17:03:49 +0330

    add base html file to run the code and write find function for BST

--- BST.js
+++ BST.js
@@ -3,46 +3,67 @@ Please implement the find and contains to the following Binary Search Tree
 **/
 
 class Node {
-    constructor(value){
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 class BinarySearchTree {
-    constructor(){
-        this.root = null;
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
     }
-    insert(value){
-        var newNode = new Node(value);
-        if(this.root === null){
-            this.root = newNode;
-            return this;
+    var current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
         }
-        var current = this.root;
-        while(true){
-            if(value === current.value) return undefined;
-            if(value < current.value){
-                if(current.left === null){
-                    current.left = newNode;
-                    return this;
-                }
-                current = current.left;
-            } else {
-                if(current.right === null){
-                    current.right = newNode;
-                    return this;
-                } 
-                current = current.right;
-            }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
         }
+        current = current.right;
+      }
     }
-    find(value){
-        // find the node based on the given value
-        // return value should be the found node or undefined
+  }
+  find(value) {
+    let nowRoot = this.root;
+    while (nowRoot !== null) {
+      if (value === nowRoot.value) {
+        return nowRoot;
+      } else if (value > nowRoot.value) {
+        nowRoot = nowRoot.right;
+      } else {
+        nowRoot = nowRoot.left;
+      }
     }
-    contains(value){
-        // return true or false if the contains the given value or not
-    }
+    return undefined;
+  }
+  contains(value) {
+    // return true or false if the contains the given value or not
+  }
 }
+
+let myNode = new BinarySearchTree();
+myNode.insert(8);
+myNode.insert(3);
+myNode.insert(10);
+myNode.insert(1);
+myNode.insert(6);
+myNode.insert(14);
+myNode.insert(4);
+myNode.insert(7);
+myNode.insert(13);
+console.log(myNode.find(6));
