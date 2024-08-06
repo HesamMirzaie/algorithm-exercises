351fd798f917487bb90cf7bd82e5c45553270d07
HesamMirzaie <mirzaeeh08@gmail.com>
2024-08-07 00:04:06 +0330

    some changes

--- maxSubarraySum.js
+++ maxSubarraySum.js
@@ -21,19 +21,19 @@ function maxSubarraySum(arr, num) {
     return null;
   }
 
-  let maxSum = 0;
-  let tempSum = 0;
+  let max = 0;
+  let currSum = 0;
   for (let i = 0; i < num; i++) {
-    maxSum += arr[i];
+    max += arr[i];
   }
-  tempSum = maxSum;
+  currSum = max;
 
   for (let i = num; i < arr.length; i++) {
-    tempSum = tempSum - arr[i - num] + arr[i];
-    maxSum = Math.max(maxSum, tempSum);
+    currSum = currSum - arr[i - num] + arr[i];
+    max = Math.max(max, currSum);
   }
 
-  return maxSum;
+  return max;
 }
 
 console.log(
